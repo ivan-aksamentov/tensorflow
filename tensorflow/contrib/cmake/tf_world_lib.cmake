0e8f4fe454ebcf7758b8a95bda085eab8b591d9b
ivan.aksamentov <ivan.aksamentov@gmail.com>
2017-03-04 15:23:20 +0100

    Add a world lib for cmake build
    This library is intended to encompass entire TensorFlow C++ API

---
+++ tensorflow/contrib/cmake/tf_world_lib.cmake
@@ -0,0 +1,18 @@
+
+add_library(tf_world_lib SHARED
+    $<TARGET_OBJECTS:tf_core_lib>
+    $<TARGET_OBJECTS:tf_core_cpu>
+    $<TARGET_OBJECTS:tf_core_framework>
+    $<TARGET_OBJECTS:tf_core_kernels>
+    $<TARGET_OBJECTS:tf_cc_framework>
+    $<TARGET_OBJECTS:tf_cc_ops>
+    $<TARGET_OBJECTS:tf_core_ops>
+    $<TARGET_OBJECTS:tf_core_direct_session>
+    $<$<BOOL:${tensorflow_ENABLE_GPU}>:$<TARGET_OBJECTS:tf_stream_executor>>
+)
+
+target_link_libraries(tf_world_lib PUBLIC
+    tf_protos_cc
+    ${tf_core_gpu_kernels_lib}
+    ${tensorflow_EXTERNAL_LIBRARIES}
+)
