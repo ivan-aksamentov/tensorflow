6f2973fcf49d925e1343c1ead2604f25575a4ba6
ivan.aksamentov <ivan.aksamentov@gmail.com>
2017-03-04 17:33:18 +0100

    Import all symbols into world lib

--- tensorflow/contrib/cmake/tf_world_lib.cmake
+++ tensorflow/contrib/cmake/tf_world_lib.cmake
@@ -16,3 +16,17 @@ target_link_libraries(tf_world_lib PUBLIC
     ${tf_core_gpu_kernels_lib}
     ${tensorflow_EXTERNAL_LIBRARIES}
 )
+
+if (WIN32)
+    set_target_properties(tf_world_lib PROPERTIES
+        LINK_FLAGS "/WHOLEARCHIVE"
+    )
+elseif (APPLE)
+    set_target_properties(tf_world_lib PROPERTIES
+        LINK_FLAGS "-Wl,-all_load"
+    )
+else()
+    set_target_properties(tf_world_lib PROPERTIES
+        LINK_FLAGS "-Wl,--whole-archive"
+    )
+endif()
